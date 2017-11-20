e9c481c30400ac23ff3058296bba23e5117abc9f
Prachi Kushwah <prkushwa@microsoft.com>
2017-11-20 14:23:07 +0530

    Add files via upload

---
+++ LICpolicy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bot_1
+{
+    [Serializable]
+    public class LICpolicy
+    {
+        public Int32 id { get; set; }
+        public string name { get; set; }
+
+        public string plan { get; set; }
+
+        public string entryAge { get; set; }
+
+        public string
+            minSumAssured { get; set; }
+
+        public string status { get; set; }
+
+        public string minPremium { get; set; }
+
+        public string image { get; set; }
+
+        public string maturityDate { get; set; }
+    }
+}
+
+
---
+++ PolicyQuery.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Bot_1
+
+{
+    using System;
+using Microsoft.Bot.Builder.FormFlow;
+
+[Serializable]
+public class PolicyQuery
+{
+   // [Numeric(0,100)]
+    [Prompt("Please enter your {&}")]
+    public string EntryAge { get; set; }
+
+
+   // [Numeric(100,int.MaxValue)]
+    [Prompt("What is the {&} you want to start with ?")]
+    public string MinimumPremium { get; set; }
+
+   // [Numeric(1, int.MaxValue)]
+    [Prompt(" What is the {&} that you expect?")]
+    public string MinimumSumAssured { get; set; }
+}
+}
